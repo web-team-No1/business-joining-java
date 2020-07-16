8118c09f3804fb17f96b0a7591410a3acc74cff2
wxGitHub1 <37536274+wxGitHub1@users.noreply.github.com>
2020-07-16 15:09:19 +0800

    ..

---
+++ src/components/return/return_variable.js
@@ -0,0 +1,403 @@
+const data_box_use = [
+  {
+    productType: 1,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceCTaxian: null,
+    experienceCKaijiao: null,
+    experienceZKaijiao: null,
+    experienceZDuanceng: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 2,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceMopifu: null,
+    experienceZimukouqiaoqi: null,
+    experienceGuanjielinghuodu: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 3,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceZhijuxiahua: null,
+    experienceLuosisongdong: null,
+    experienceZimukouqiaoqi: null,
+    experienceMopifu: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 4,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceLuosisongdong: null,
+    experienceZimukouqiaoqi: null,
+    experienceMopifu: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 5,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceMopifu: null,
+    experienceZhijuxiahua: null,
+    experienceZimukouqiaoqi: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 6,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceJiaodusongdong: null,
+    experienceMopifu: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 7,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceMopifu: null,
+    experienceJiaodubianhua: null,
+    experienceButai: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  },
+  {
+    productType: 8,
+    experienceUseTime: null,
+    experienceSatisfaction: null,
+    experienceUseEffect: null,
+    experienceNotice: [],
+    experienceZimukouqiaoqi: null,
+    experienceMopifu: null,
+    experienceQita: null,
+    experienceProblemHave: null,
+    experienceProblemDo: null
+  }
+]
+const data_box = [
+  {
+    productType: 1,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useDicengkailie: null,
+    useUseEffect: null,
+    useCTaxian: null,
+    useMopifu: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 2,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useNilongdai: null,
+    useZhijubianxing: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 3,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useNilongdai: null,
+    useZhijubianxing: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 4,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useNilongdai: null,
+    useLuosisongdong: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 5,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useNilongdai: null,
+    useZhijubianxing: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 6,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useNilongdai: null,
+    useJiaodusongdong: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 7,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useLianjiechutuokai: null,
+    useButai: null,
+    useProblemHave: null,
+    useProblemDo: null
+  },
+  {
+    productType: 8,
+    useUseTime: null,
+    useHeightWeight: null,
+    useSatisfaction: null,
+    useNotice: [],
+    useClean: null,
+    useUseEffect: null,
+    useMopifu: null,
+    useNilongdai: null,
+    useZimukoushengxiu: null,
+    useProblemHave: null,
+    useProblemDo: null
+  }
+]
+const productItem = {
+  item_1: false,
+  item_2: false,
+  item_3: false,
+  item_4: false,
+  item_5: false,
+  item_6: false,
+  item_7: false,
+  item_8: false
+}
+//体验回访详情
+const productItemUse = {
+  item_1: false,
+  item_2: false,
+  item_3: false,
+  item_4: false,
+  item_5: false,
+  item_6: false,
+  item_7: false,
+  item_8: false
+}
+const data_assignment = {
+  data_assignment_Dialg: false,
+  loading: true,
+  clientData: [],
+  multipleSelection: [],
+  pages: {
+    total: 10,
+    pageSize: 10,
+    currentPage: 1
+  },
+  userList: [],
+  search: {
+    time: null,
+    user: null,
+    productName: null,
+  },
+  experience_details_dialog_two: false,
+  zpUser: null
+}
+const new_details_data = {
+  obj: {},
+  experience_details_dialog: false,
+  prescriptionDTO: [],
+  examinationInfo: [{}],
+  relationship: null,
+  detailList: [],
+  templateData: [
+    {
+        experienceUseTime: null,
+        experienceSatisfaction: null,
+        experienceUseEffect: null,
+        experienceNotice: [],
+        experienceCTaxian: null,
+        experienceCKaijiao: null,
+        experienceZKaijiao: null,
+        experienceZDuanceng: null,
+        experienceProblemHave: null,
+        experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceMopifu: null,
+      experienceZimukouqiaoqi: null,
+      experienceGuanjielinghuodu: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceZhijuxiahua: null,
+      experienceLuosisongdong: null,
+      experienceZimukouqiaoqi: null,
+      experienceMopifu: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceLuosisongdong: null,
+      experienceZimukouqiaoqi: null,
+      experienceMopifu: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceMopifu: null,
+      experienceZhijuxiahua: null,
+      experienceZimukouqiaoqi: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceJiaodusongdong: null,
+      experienceMopifu: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceMopifu: null,
+      experienceJiaodubianhua: null,
+      experienceButai: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    },
+    {
+      experienceUseTime: null,
+      experienceSatisfaction: null,
+      experienceUseEffect: null,
+      experienceNotice: [],
+      experienceZimukouqiaoqi: null,
+      experienceMopifu: null,
+      experienceQita: null,
+      experienceProblemHave: null,
+      experienceProblemDo: null
+    }
+  ],
+  td_dialog:false,
+  value:0,
+  causeOfLoss:null,
+  churnRegistration:null,
+  ls_dialog:false
+}
+const fayy_data={
+  dayList:[
+    {name:"上午",id:1},
+    {name:"下午",id:2},
+  ],
+  dayValue:null,
+  timeList:[
+    {name:"1点",id:1},
+    {name:"2点",id:2},
+    {name:"3点",id:3},
+    {name:"4点",id:4},
+    {name:"5点",id:5},
+    {name:"6点",id:6},
+    {name:"7点",id:7},
+    {name:"8点",id:8},
+    {name:"9点",id:9},
+    {name:"10点",id:10},
+    {name:"11点",id:11},
+    {name:"12点",id:12},
+  ],
+  timeValue:null,
+  minuteList:[
+    {name:"5分",id:5},
+    {name:"10分",id:10},
+    {name:"15分",id:15},
+    {name:"20分",id:20},
+    {name:"25分",id:15},
+    {name:"30分",id:30},
+    {name:"35分",id:35},
+    {name:"40分",id:40},
+    {name:"45分",id:45},
+    {name:"50分",id:50},
+    {name:"55分",id:55},
+    {name:"60分",id:60},
+  ],
+  minuteValue:null,
+}
+export default {
+  data_box,
+  data_box_use,
+  productItem,
+  productItemUse,
+  data_assignment,
+  new_details_data,
+  fayy_data,
+}
